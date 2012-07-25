7d0932e0eb1c9062246712729d507c7ab659997b
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-07-25 13:42:41 +0200

    Option to hide Adb notification icon (framework)
    Change-Id: I023ef8454955eef93f36ede467e2da0da07c176c
    Signed-off-by: Rohan Mathur <rohanmathur34@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6871,6 +6871,12 @@ public final class Settings {
          */
         public static final String SHOW_STATUSBAR_IME_SWITCHER = "show_statusbar_ime_switcher";
 
+        /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
     }
 
     /**

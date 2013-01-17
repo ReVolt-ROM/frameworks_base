f4a73adc4d4efbb30954338abd9f3179c88e81f5
Jesús David <jedga_95@outlook.com>
2013-01-17 16:26:57 -0500

    A couple of fixes
    * Fix derp causing pie to be always visible without setting influence
    * Gravity changes are now instant
    * AM / PM is set into its right place, remove listeners from clock class
    * Wifi always uppercase

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -80,17 +80,6 @@ public class Clock extends TextView {
 
     protected int mClockColor;
 
-    private OnClockChangedListener mClockChangedListener;
-
-    public interface OnClockChangedListener {
-        public abstract void onChange(CharSequence t);
-    }
-
-    class SettingsObserver extends ContentObserver {
-        SettingsObserver(Handler handler) {
-            super(handler);
-        }
-
     public Clock(Context context) {
         this(context, null);
     }
@@ -101,6 +90,14 @@ public class Clock extends TextView {
 
     public Clock(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        mContext = context;
+        TypedArray a = context.obtainStyledAttributes(attrs, com.android.systemui.R.styleable.Clock, defStyle, 0);
+        mShowAlways = a.getBoolean(com.android.systemui.R.styleable.Clock_showAlways, false);
+
+        updateParameters();
+
+        SettingsObserver observer = new SettingsObserver(new Handler());
+        observer.observe();
     }
 
     public void startBroadcastReceiver() {
@@ -127,10 +124,6 @@ public class Clock extends TextView {
         SettingsObserver settingsObserver = new SettingsObserver(new Handler());
         settingsObserver.observe();
         updateSettings();
-    }
-
-    public void setOnClockChangedListener(OnClockChangedListener l){
-        mClockChangedListener = l;
     }
 
     @Override
@@ -169,13 +162,18 @@ public class Clock extends TextView {
         }
     };
 
+    private void updateParameters() {
+        setVisibility((Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_CLOCK, 1) == 1) ? View.VISIBLE : View.GONE);
+        AM_PM_STYLE = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_AM_PM_STYLE, 2);
+        mClockFormatString = null;
+    }
+
     final void updateClock() {
         mCalendar.setTimeInMillis(System.currentTimeMillis());
         CharSequence seq = getSmallTime();
         setText(seq);
-        if (mClockChangedListener != null) {
-            mClockChangedListener.onChange(seq);
-        }
     }
 
     public final CharSequence getSmallTime() {
@@ -273,8 +271,13 @@ public class Clock extends TextView {
 
         @Override
         public void onChange(boolean selfChange) {
+            if(!mShowAlways) updateParameters();
             updateSettings();
         }
+    }
+
+    private void updateParameters() {
+
     }
 
     protected void updateSettings() {

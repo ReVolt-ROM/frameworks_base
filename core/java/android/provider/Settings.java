8d6b5dd33acc3519182e260b7080eb5c074d89a8
drcmda <drcmda@gmail.com>
2013-01-18 12:26:47 +0000

    clean-up, new options, getting rid of unneeded code
    Change-Id: I62092bd712f962d67e9734a1bdf0d27bee61c8d9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2894,6 +2894,13 @@ public final class Settings {
         public static final String PIE_CONTROLS = "pie_controls";
 
         /**
+         * Whether to enable or not pie per-app-coloring
+         * The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String PIE_PAC = "pie_pac";
+
+        /**
          * Location of the pie in the screen
          * 0 = Gravity.TOP
          * 1 = Gravity.BOTTOM
@@ -2902,6 +2909,16 @@ public final class Settings {
          * @hide
          */
         public static final String PIE_GRAVITY = "pie_gravity";
+
+        /**
+         * Pie status report
+         * 0 = Bare
+         * 1 = Quick
+         * 2 = Default
+         * 3 = Slow
+         * @hide
+         */
+        public static final String PIE_MODE = "pie_mode";
 
         /**
          * whether to hide the signal barss

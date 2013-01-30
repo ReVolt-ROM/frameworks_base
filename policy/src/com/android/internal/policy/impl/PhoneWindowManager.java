4226e740fb5cf156fd5fccf12972a63e5fc91189
Daniel <daxx4182@gmail.com>
2013-01-30 22:47:22 +0800

    Pie Color (The noob way) (1/2)

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -158,6 +158,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 /**
  * WindowManagerPolicy implementation for the Android phone UI.  This

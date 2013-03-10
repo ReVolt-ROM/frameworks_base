dd89385716ebcb105c3571d4592f8a893f77a838
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-10 05:58:43 +0100

    SystemUI: Screenshot Toggle
    onClick: Screenshot now(collpase status bar)
    onLongClick: Starts 'timer' and screenshot after timer expires
    
    PS2: Remove rebootmenu calls from PWM and add 500ms delay to allow statusbar to
             fully collapse
    
    PS3,4: Redependcize commits
    PS5: Secure Screenshot
    PS6: Resolved conflicts
    PS8: Respect RC delay setting & fix drawable padding
    PS9: Removed to-do
    PS10: collapseStatusBar() for longclick and added toast
    PS11: reduce toast length to avoid capturing it. also make it translatable
    
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: I2cebb23e530a92901319b7171c3934125568c3ab

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -93,6 +93,7 @@ public class ToggleManager {
     public static final String QUIETHOURS_TOGGLE = "QUIETHOURS";
     public static final String SLEEP_TOGGLE = "SLEEP";
     public static final String PIE_TOGGLE = "PIE";
+    public static final String SCREENSHOT_TOGGLE = "SCREENSHOT";
 
     private int mStyle;
 
@@ -147,6 +148,7 @@ public class ToggleManager {
             toggleMap.put(QUIETHOURS_TOGGLE, QuietHoursToggle.class);
             toggleMap.put(SLEEP_TOGGLE, SleepToggle.class);
             toggleMap.put(PIE_TOGGLE, PieToggle.class);
+            toggleMap.put(SCREENSHOT_TOGGLE, ScreenshotToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;

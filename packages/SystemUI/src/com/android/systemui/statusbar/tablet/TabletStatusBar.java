b75c8e868afed3f891bc09ed9b1d0ea43f927893
John Hany <johnhany97@gmail.com>
2013-04-07 16:21:48 +0300

    Revert "TABUI: Apply redraw fix"
    This reverts commit a19c71c58c012fd4164d82c153bb002739fa40ce.

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -206,7 +206,6 @@ public class TabletStatusBar extends BaseStatusBar implements
     KeyEvent mSpaceBarKeyEvent = null;
 
     View mCompatibilityHelpDialog = null;
-    View mDummyView ;
 
     // for disabling the status bar
     int mDisabled = 0;
@@ -494,10 +493,6 @@ public class TabletStatusBar extends BaseStatusBar implements
         mStatusBarView = sb;
 
         sb.setHandler(mHandler);
-
-        // Create a dummy view to force the screen to redraw
-        mDummyView = new View(mContext);
-        mWindowManager.addView(mDummyView, getDummyTriggerLayoutParams(mContext,Gravity.TOP));
 
         mBarContents = (ViewGroup) sb.findViewById(R.id.bar_contents);
 
@@ -1649,8 +1644,7 @@ public class TabletStatusBar extends BaseStatusBar implements
         // If the device hasn't been through Setup, we only show system notifications
         for (int i=0; i<N; i++) {
             Entry ent = mNotificationData.get(N-i-1);
-            if (provisioned || showNotificationEvenIfUnprovisioned(ent.notification)
-                && !notificationIsForCurrentUser(ent.notification)) {
+            if (provisioned || showNotificationEvenIfUnprovisioned(ent.notification)) {
                 toShow.add(ent.row);
             }
         }

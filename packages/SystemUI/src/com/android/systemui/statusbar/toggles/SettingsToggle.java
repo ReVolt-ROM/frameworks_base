6335715cc3decd217b4dbbd76bbd7c54400e6381
John Hany <johnhany97@gmail.com>
2013-04-23 11:41:39 +0300

    Point back to ROM Control

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/SettingsToggle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/SettingsToggle.java
@@ -31,7 +31,7 @@ public class SettingsToggle extends BaseToggle {
     public boolean onLongClick(View v) {
         Intent intent = new Intent("android.intent.action.MAIN");
         intent.setComponent(ComponentName
-                .unflattenFromString("com.revolt.control/.ReVoltControlActivity"));
+                .unflattenFromString("com.aokp.romcontrol/.ROMControlActivity"));
         intent.addCategory("android.intent.category.LAUNCHER");
 
         startActivity(intent);

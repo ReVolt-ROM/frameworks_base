60d535ae7fccdc3cae967869cc69fa4f55b69556
John Hany <johnhany97@gmail.com>
2013-05-10 00:54:03 +0300

    Revert "SystemUI: StayAwake Toggle"
    This reverts commit aa0edbe1db12aef0b616cda5b80e377c7b9070ee.

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -102,7 +102,6 @@ public class ToggleManager {
     public static final String REBOOT_TOGGLE = "REBOOT";
     public static final String CUSTOM_TOGGLE = "CUSTOM";
     public static final String DARK_TOGGLE = "DARK";
-    public static final String STAYAWAKE_TOGGLE = "STAYAWAKE";
 
     private int mStyle;
 
@@ -176,7 +175,6 @@ public class ToggleManager {
             toggleMap.put(REBOOT_TOGGLE, RebootToggle.class);
             toggleMap.put(CUSTOM_TOGGLE, CustomToggle.class);
             toggleMap.put(DARK_TOGGLE, DarkToggle.class);
-            toggleMap.put(STAYAWAKE_TOGGLE, StayAwakeToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;

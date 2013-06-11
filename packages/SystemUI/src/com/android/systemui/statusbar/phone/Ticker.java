cdae5a976f2d1fa89e8581de1abb9366705e80cd
drcmda <drcmda@gmail.com>
2013-06-11 12:59:09 +0100

    HALO cleanup
    hdpi and mdpi drawables were inconsistent, if you plan to use HALO on
    lower res devices you must reproduce them.
    
    Change-Id: I2746837bacd60c3079ace4c7523a68b8cd415f5f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
@@ -317,7 +317,7 @@ public abstract class Ticker {
 
     private void scheduleAdvance() {
         mHandler.postDelayed(mAdvanceTicker, TICKER_SEGMENT_DELAY);
-    }    
+    }
 
     public abstract void tickerStarting();
     public abstract void tickerDone();

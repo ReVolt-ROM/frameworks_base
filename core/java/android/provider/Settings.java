e6904db7206abbe4ee9519b33b1a7d52145f5bb2
Daniel MM <daxx4182@gmail.com>
2013-06-15 14:38:53 +0800

    HALO minimal style (1/2)
    TODO: Bring more styles and turn it into a list...

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3832,6 +3832,13 @@ public final class Settings {
 
         /**
          *
+         * HALO being minimal.
+         * @hide
+         */
+        public static final String HALO_STYLE = "halo_style";
+
+        /**
+         *
          * @hide
          */
         public static final String APP_WINDOW_HIDDEN_APPS = "app_window_hidden_apps";

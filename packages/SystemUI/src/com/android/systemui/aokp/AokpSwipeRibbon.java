4e02a29b09dc6e9c3efd35c05fec45f5d7daaa11
Daniel MM <daxx4182@gmail.com>
2013-06-26 00:09:14 +0800

    Toggle: Music Toggle
    A port from Slim and CyanogenMod.
    
    Kudos to them. :)
    
    Conflicts:
    
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/aokp/AokpSwipeRibbon.java

--- packages/SystemUI/src/com/android/systemui/aokp/AokpSwipeRibbon.java
+++ packages/SystemUI/src/com/android/systemui/aokp/AokpSwipeRibbon.java
@@ -483,6 +483,7 @@ public class AokpSwipeRibbon extends LinearLayout {
             toggleMap.put(DARK_TOGGLE, DarkToggle.class);
             toggleMap.put(STAYAWAKE_TOGGLE, StayAwakeToggle.class);
             toggleMap.put(WIRELESS_ADB_TOGGLE, WirelessAdbToggle.class);
+            toggleMap.put(MUSIC_TOGGLE, MusicToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;

8b94b9cf301a3c5a11781ddf19a62346cbede445
htchoi1995 <htchoi1995@gmail.com>
2013-07-11 01:03:45 +0900

    SystemUI: Fix NPE on recent panel
    Change-Id: I40bbbfc825aebcb601fc80d1e27c346285ebcdf6

--- packages/SystemUI/src/com/android/systemui/recent/RecentsHorizontalScrollView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsHorizontalScrollView.java
@@ -221,7 +221,9 @@ public class RecentsHorizontalScrollView extends HorizontalScrollView
     public void onChildDismissed(View v, boolean fromUser) {
         addToRecycledViews(v);
         mLinearLayout.removeView(v);
-        mCallback.handleSwipe(v);
+        if (mCallback != null) {
+        	mCallback.handleSwipe(v);
+        }
         // Restore the alpha/translation parameters to what they were before swiping
         // (for when these items are recycled)
         View contentView = getChildContentView(v);

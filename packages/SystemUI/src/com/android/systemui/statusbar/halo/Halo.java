b892919bd87e2444c378e303f628ca610b20e508
drcmda <drcmda@gmail.com>
2013-07-31 21:45:14 +0100

    HALO smoother operation
    Change-Id: I8c906e969077c826f168a2c452d56d2ce49f78a8
    
    Conflicts:
    
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
    	packages/SystemUI/src/com/android/systemui/statusbar/halo/HaloProperties.java

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -1260,7 +1260,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
         mEffect.invalidate();
 
         // Set Number
-        mEffect.setHaloMessageNumber(n.number, alwaysFlip);
+        mEffect.setHaloMessageNumber(n.number, alwaysFlip, delay);
     }
 
     public void updateTicker(StatusBarNotification notification) {
@@ -1296,7 +1296,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
                     mNotificationText = text;
                     mLastNotificationEntry = entry;
 
-                    if (allowed) {
+                    if (allowed && mState != State.FIRST_RUN) {
                         tick(entry, text, HaloEffect.WAKE_TIME, 1000, true);
 
                         // Pop while not tasking, only if notification is certified fresh
@@ -1307,9 +1307,14 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback, TabletTi
                         }
                         if (mState == State.IDLE || mState == State.HIDDEN) {
                             mEffect.wake();
-                            mEffect.nap(HaloEffect.NAP_DELAY);
-                            if (mHideTicker) mEffect.sleep(HaloEffect.SLEEP_DELAY, HaloEffect.SLEEP_TIME, false);
+                            mEffect.nap(HaloEffect.NAP_DELAY + HaloEffect.WAKE_TIME * 2);
+                            if (mHideTicker) mEffect.sleep(HaloEffect.SLEEP_DELAY + HaloEffect.WAKE_TIME * 2, HaloEffect.SLEEP_TIME, false);
                         }
+
+                        tick(entry, text, HaloEffect.WAKE_TIME * 3, 1000, true);
+
+                        // Pop while not tasking, only if notification is certified fresh
+                        if (mGesture != Gesture.TASK && mState != State.SILENT) mEffect.ping(mPaintHoloBlue, HaloEffect.WAKE_TIME * 2);
                     }
                 }
                 break;

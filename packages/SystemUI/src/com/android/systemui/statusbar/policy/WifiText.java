945988cbc034d5ec26d97c59bc1e360ed94cbce6
John Hany <johnhany97@gmail.com>
2013-09-07 23:49:35 +0300

    Attempt to Fix Per-App-Color for WiFiText.java
    Needs testing though

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/WifiText.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/WifiText.java
@@ -1,4 +1,6 @@
 package com.android.systemui.statusbar.policy;
+
+import android.R.integer;
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -6,8 +8,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.res.Resources;
 import android.database.ContentObserver;
+import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Handler;
+import android.os.Message;
 import android.provider.Settings;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
@@ -15,6 +19,8 @@ import android.text.style.CharacterStyle;
 import android.text.style.RelativeSizeSpan;
 import android.util.AttributeSet;
 import android.util.ColorUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class WifiText extends TextView {
@@ -22,21 +28,22 @@ public class WifiText extends TextView {
     private static final String TAG = "WiFiText";
     private int mRssi;
     private boolean mAttached;
+    private boolean mInAirplaneMode;
     private static final int STYLE_HIDE = 0;
     private static final int STYLE_SHOW = 1;
     private int style;
     private Handler mHandler;
     private Context mContext;
     private WifiManager mWifiManager;
-    protected int mSignalColor = com.android.internal.R.color.holo_blue_light;
-
-    private ColorUtils.ColorSettingInfo mLastTextColor;
+    protected int mSignalColor;
 
     /* Anything worse than or equal to this will show 0 bars. */
     private static final int MIN_RSSI = -100;
 
     /* Anything better than or equal to this will show the max bars. */
     private static final int MAX_RSSI = -55;
+
+    private ColorUtils.ColorSettingInfo mLastTextColor;
 
     private SettingsObserver mSettingsObserver;
 
@@ -101,7 +108,7 @@ public class WifiText extends TextView {
 
         if (!mAttached) {
             mAttached = true;
-            mSignalColor = getTextColors().getDefaultColor();
+	    mSignalColor = getTextColors().getDefaultColor();
             mHandler = new Handler();
             mSettingsObserver = new SettingsObserver(mHandler);
             mSettingsObserver.observe();
@@ -134,6 +141,7 @@ public class WifiText extends TextView {
             resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.STATUSBAR_WIFI_SIGNAL_TEXT_COLOR), false,
                     this);
+            updateSettings();
         }
 
         @Override
@@ -143,15 +151,14 @@ public class WifiText extends TextView {
     }
 
     private void updateSettings() {
+	int newColor = 0;
         ContentResolver resolver = getContext().getContentResolver();
-        mSignalColor = Settings.System.getInt(resolver,
-                Settings.System.STATUSBAR_WIFI_SIGNAL_TEXT_COLOR,
-                0xFF33B5E5);
-        if (mSignalColor == Integer.MIN_VALUE) {
-            // flag to reset the color
-            mSignalColor = 0xFF33B5E5;
+        newColor = Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_WIFI_SIGNAL_TEXT_COLOR,mSignalColor);
+        if (newColor < 0 && newColor != mSignalColor) {
+            mSignalColor = newColor;
+            setTextColor(mSignalColor);
         }
-        setTextColor(mSignalColor);
         updateSignalText();
     }
 

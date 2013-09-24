bb0a04f3eb17bba2973ef3082f607bba22911c03
Abhisek Devkota <ciwrl@cyanogenmod.com>
2013-09-24 03:36:52 +0000

    Merge "Revert "framework: Privacy Guard"" into cm-10.2 Conflicts: 	core/java/android/app/ApplicationPackageManager.java 	core/java/android/provider/Settings.java 	core/res/res/drawable-hdpi/stat_notify_privacy_guard.png 	core/res/res/drawable-mdpi/stat_notify_privacy_guard.png 	core/res/res/drawable-xhdpi/stat_notify_privacy_guard.png 	core/res/res/drawable-xxhdpi/stat_notify_privacy_guard.png 	core/res/res/values/cm_strings.xml 	core/res/res/values/symbols.xml 	services/java/com/android/server/pm/Settings.java

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -48,7 +48,6 @@ import android.content.res.Resources;
 import android.content.res.XmlResourceParser;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Binder;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.UserHandle;
@@ -1307,43 +1306,6 @@ final class ApplicationPackageManager extends PackageManager {
         return PackageManager.COMPONENT_ENABLED_STATE_DEFAULT;
     }
 
-    @Override
-
-    public String[] getRevokedPermissions(String packageName) {
-        try {
-            return mPM.getRevokedPermissions(packageName);
-        } catch (RemoteException e) {
-            // Should never happen!
-        }
-        return new String[0];
-    }
-
-    @Override
-    public void setRevokedPermissions(String packageName, String[] perms) {
-        try {
-            mPM.setRevokedPermissions(packageName, perms);
-        } catch (RemoteException e) {
-            // Should never happen!
-        }
-    }
-
-    public void setPrivacyGuardSetting(String packageName, boolean enabled) {
-        try {
-            mPM.setPrivacyGuardSetting(packageName, enabled, mContext.getUserId());
-        } catch (RemoteException e) {
-            // Should never happen!
-        }
-    }
-
-    @Override
-    public boolean getPrivacyGuardSetting(String packageName) {
-        try {
-            return mPM.getPrivacyGuardSetting(packageName, mContext.getUserId());
-        } catch (RemoteException e) {
-            // Should never happen!
-        }
-        return false;
-    }
 
     /**
      * @hide

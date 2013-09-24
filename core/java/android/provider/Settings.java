7937f49f10f4bafacff5ff2d370a0cf18dbc7ee1
Abhisek Devkota <ciwrl@cyanogenmod.com>
2013-09-24 03:37:05 +0000

    Merge "Privacy Guard : Reimplement backed by AppOps" into cm-10.2 Conflicts: 	core/res/res/drawable-hdpi/stat_notify_privacy_guard.png 	core/res/res/drawable-mdpi/stat_notify_privacy_guard.png 	core/res/res/drawable-xhdpi/stat_notify_privacy_guard.png 	core/res/res/drawable-xxhdpi/stat_notify_privacy_guard.png 	core/res/res/values/cm_strings.xml 	services/java/com/android/server/pm/PackageManagerService.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6829,6 +6829,12 @@ public final class Settings {
         public static final String ENABLE_PERMISSIONS_MANAGEMENT = "enable_permissions_management";
 
         /**
+         * Whether newly installed apps should run with privacy guard by default
+         * @hide
+         */
+         public static final String PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
@@ -6876,7 +6882,8 @@ public final class Settings {
             DIALPAD_AUTOCOMPLETE,
             LOCK_SCREEN_OWNER_INFO,
             LOCK_SCREEN_OWNER_INFO_ENABLED,
-            ADVANCED_REBOOT
+            ADVANCED_REBOOT,
+            PRIVACY_GUARD_DEFAULT
         };
 
         /**

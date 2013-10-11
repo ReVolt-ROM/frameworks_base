fd7a94aa5fe29e4fae4c674a06918f363e8509fe
helicopter88 <johnlemon.lol@gmail.com>
2013-10-11 20:32:51 +0400

    Frameworks: Policy: implement a pin keypad shuffler [1/2]
    this is to further prevent people using the tablet guessing your pin based on fingerprints left on the screen
    example of the expected outcome - https://www.dropbox.com/sh/r3xb2q53ksaqni9/Oz1Eu8Dm7O
    
    - correct coding style
    - added reload button
    
    PatchSet:
    - simplify shuffle code
    - fix over shifting in some cases
    - add reload button functionallity
    - add lockscreenutils method to detect only PIN lock
      for settings
    - exclude sim and simpuk PIN lock
    - add drawables for reload button
    
    PatchSet
    - cleanup
    
    PatchSet:
    - fix shuffle NumKeyPad even if a theme replaces the xml
      and reload button is not present.
    
    PatchSet:
    - add mode disabled/always/on request
    
    cheers
    
    kufi
    
    Change-Id: I6471a531b71fafb900a185f0f96c6188d1f2caa7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -968,6 +968,7 @@ public final class Settings {
             MOVED_TO_SECURE.add(Secure.LOCK_DOTS_VISIBLE);
             MOVED_TO_SECURE.add(Secure.LOCK_PATTERN_TACTILE_FEEDBACK_ENABLED);
             MOVED_TO_SECURE.add(Secure.LOCK_BEFORE_UNLOCK);
+            MOVED_TO_SECURE.add(Secure.LOCK_NUMPAD_RANDOM);
             MOVED_TO_SECURE.add(Secure.LOCKSCREEN_UNSECURE_USED);
             MOVED_TO_SECURE.add(Secure.LOGGING_ID);
             MOVED_TO_SECURE.add(Secure.PARENTAL_CONTROL_ENABLED);
@@ -6065,6 +6066,16 @@ public final class Settings {
          */
         public static final String LOCK_PATTERN_SIZE =
             "lock_pattern_size";
+
+        /**
+         * Whether the NumKeyPad will change the orders of numbers
+         * in a PIN locked lockscreen
+         * 0 = off | 1 = always | 2 = only on request
+         * @hide
+          */
+
+        public static final String LOCK_NUMPAD_RANDOM =
+            "lock_numpad_random";
 
         /**
          * Whether to sync encryption password with lock screen token

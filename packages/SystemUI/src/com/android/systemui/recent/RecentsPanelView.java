2e0c9d50e767536dfdc5dece3c11bf15e94c8bce
nagarw <nagarw@codeaurora.org>
2013-10-11 15:37:22 +0530

    /platform/frameworks/base: Fix SystemUI crash on Recent App
    Issue: SystemUI crash is observed when Context MenuItem is
           selected on the recent apps activity [Long pressing
           two recent apps at same time or Long press, lock and
           unlock the screen]
    
    Fix: Changed the code to make sure that only one context
         menu is active at a time and also dismissed the
         context menu on pause of the RecentsActivity
    
    CRs-Fixed: 539509
    
    Change-Id: Ia50bed7bfad6fde7beeaec37fa6402177eee1d40

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -368,6 +368,11 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         }
     }
 
+    public void dismissContextMenuIfAny() {
+        if(mPopup != null) {
+            mPopup.dismiss();
+        }
+    }
     public void show(boolean show) {
         show(show, null, false, false);
     }
@@ -881,6 +886,9 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
 
     public void handleLongPress(
             final View selectedView, final View anchorView, final View thumbnailView) {
+        if(mPopup != null) {
+            mPopup.dismiss();
+        }
         thumbnailView.setSelected(true);
         final PopupMenu popup =
             new PopupMenu(mContext, anchorView == null ? selectedView : anchorView);

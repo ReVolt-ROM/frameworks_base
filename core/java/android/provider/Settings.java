c2bd1eae72fab9e131edcfbb8cacabd5923242ad
John Hany <johnhany97@gmail.com>
2014-01-19 16:50:39 -0500

    Revert "base: show notification count overlay"
    This reverts commit 25e593e9dc96a601fa022276c098c1e471cde5f7.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2519,12 +2519,6 @@ public final class Settings {
         public static final String EGG_MODE = "egg_mode";
 
         /**
-         * Show the pending notification counts as overlays on the status bar
-         * @hide
-         */
-        public static final String STATUS_BAR_NOTIF_COUNT = "status_bar_notif_count";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

15712d02449c5eadd3f21310feabd8211882d0ae
riddle_hsu <riddle_hsu@htc.com>
2014-07-04 17:01:56 +0800

    [ActivityManager] Avoid keeping restarting home when only home activity exists.
    Root Cause:
    When there is only home activity existed,
    updating home apk will call forceStopPackageLocked to finish the activity.
    Then activity history becomes empty, then home will be launched,
    but its package is still target to close that results a loop.
    
    Solution:
    If home activity has been force-stopped, do not stop the same home activity again.
    
    Change-Id: Icff12028d407873c2e6f50a06bcad231b908ccbd

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -3410,6 +3410,7 @@ final class ActivityStack {
     boolean forceStopPackageLocked(String name, boolean doit, boolean evenPersistent, int userId) {
         boolean didSomething = false;
         TaskRecord lastTask = null;
+        ComponentName homeActivity = null;
         for (int taskNdx = mTaskHistory.size() - 1; taskNdx >= 0; --taskNdx) {
             final ArrayList<ActivityRecord> activities = mTaskHistory.get(taskNdx).mActivities;
             int numActivities = activities.size();
@@ -3427,6 +3428,14 @@ final class ActivityStack {
                             continue;
                         }
                         return true;
+                    }
+                    if (r.isHomeActivity()) {
+                        if (homeActivity != null && homeActivity.equals(r.realActivity)) {
+                            Slog.i(TAG, "Skip force-stop again " + r);
+                            continue;
+                        } else {
+                            homeActivity = r.realActivity;
+                        }
                     }
                     didSomething = true;
                     Slog.i(TAG, "  Force finishing activity " + r);
